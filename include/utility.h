ddd9aa2dc57366f0c3ac4f298d52c64cd8e133ce
eddsanity <eyadhossamm@gmail.com>
2020-02-07 21:50:06 +0200

    implemented utilities, format function still erroneous

--- include/utility.h
+++ include/utility.h
@@ -1,19 +1,22 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include "string.h"     // needed for `char* strncat`
+#include <stdio.h>
+
 typedef enum { false, true } bool;
 
 // Just adds the explicit concatenation operator in the 'unformated input'.
 char*
-re_format(char* ufInput);
+re_format(char*);
 
 /* Checks if `c` is an operator. Returns true if it is, false otherwise.
  * operators are: `|`, `?`, `+`, `*`, `^` and `.` */
 bool
-re_isOperator(char c);
+re_isOperator(char);
 
 // returns the precedence of the input operator.
 int
-re_getPrecedence(char op);
+re_getPrecedence(char);
 
 #endif
--- src/utility.c
+++ src/utility.c
@@ -1,1 +1,53 @@
 #include "utility.h"
+
+char*
+re_format(char* ufInput)
+{
+    const char dot = '.';
+    char* output;
+    int in_length = strlen(ufInput);
+
+    for(int i = 0; i < in_length; ++i)
+    {
+	char current_char = ufInput[i];
+
+	// if current_char isn't the last character in ufInput
+	if(i + 1 < in_length)
+	{
+	    char next_char = ufInput[i+1];
+	    strncat(output, &current_char, 1);
+
+	    if(current_char != '(' && next_char != ')' && !re_isOperator(next_char))
+		strncat(output, &dot, 1);
+	}
+    }
+    strncat(output, ufInput[in_length - 1], 1);
+
+    return output;
+}
+
+
+bool
+re_isOperator(char c)
+{
+    if(c == '+' || c == '^' || c == '*' || c == '|' || c == '?' || c == '.' || c == '(')
+	return true;
+    else return false;
+}
+
+
+// lower numbers are higher precedence
+int
+re_getPrecedence(char op)
+{
+    if(op == '(')
+	return 0;
+    if(op == '|')
+	return 1;
+    if(op == '.')
+	return 2;
+    if(op == '?' || op == '*' || op == '+')
+	return 3;
+    if(op == '^')
+	return 4;
+}
