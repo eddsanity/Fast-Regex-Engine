5c445b7fbadd124be930ca14cfd8b8197f72d77a
eddsanity <eyadhossamm@gmail.com>
2020-02-11 18:31:52 +0200

    fixed tests, formatted tests and added infix2postfix tests

--- tests/infix2postfix_tests.c
+++ tests/infix2postfix_tests.c
@@ -1,4 +1,4 @@
-#include "../include/utility.h"
+#include "../include/regex.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -8,17 +8,17 @@ void
 main()
 {
     char* test_cases[NUMBER_OF_TESTS] = {"abc", "ab|c", "ab+c", "a(bb)+c", "abc(aa*)"};
-    char* accepted_output[NUMBER_OF_TESTS] = {"a.b.c", "a.b|c", "a.b+.c", "a.(b.b)+.c", "a.b.c.(a.a*)"};
+    char* accepted_output[NUMBER_OF_TESTS] = {"ab.c.", "ab.c|", "ab+.c.", "abb.+.c.", "ab.c.aa*.."};
 
     int N = 0;
     while(N < NUMBER_OF_TESTS)
     {
-	if(strcmp(re_format(test_cases[N]), accepted_output[N]) == 0)
+	if(strcmp(re_infix2postfix(test_cases[N]), accepted_output[N]) == 0)
 	    printf("TEST %d %-3s\n", N+1, "PASSED");
 	else
 	    printf("TEST %d %-3s\n", N+1, "FAILED");
 	printf("%-10s: %s\n", "INPUT", test_cases[N]);
-	printf("%-10s: %s\n", "OUTPUT", re_format(test_cases[N]));
+	printf("%-10s: %s\n", "OUTPUT", re_infix2postfix(test_cases[N]));
 	printf("%-10s: %s\n", "EXPECTED", accepted_output[N]);
 	N++;
     }
